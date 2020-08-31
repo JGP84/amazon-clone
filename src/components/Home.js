ce90e012f15e7444fcd521535b57e0bdb63f6e51
JGP84 <josegilabertpons@hotmail.com>
2020-08-31 19:24:01 +0200

    new header and images products

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,63 +6,63 @@ function Home() {
   return (
     <div className="home">
       <img
-        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg"
         className="home__image"
         alt="banner"
       />
       <div className="home__row">
         <Product
           id="0001"
-          title="Harry Potter and the Philosopher's Stone"
-          price={10.49}
+          title="AmazonBasics Book Safe, Key Lock, Small, Blue"
+          price={13.99}
           rating={5}
-          image="https://m.media-amazon.com/images/I/51UoqRAxwEL.jpg"
+          image="https://m.media-amazon.com/images/I/91E1LbRmTYL._AC_UL320_.jpg"
         />
         <Product
           id="0002"
-          title="Funny Costumes Dinosaur Inflatable Costumes"
-          price={34.84}
+          title="AmazonBasics Stainless Steel Portable Fast, Electric Hot Water Kettle for Tea and Coffee, 1 Liter, Silver"
+          price={21.99}
           rating={4}
-          image="https://images-na.ssl-images-amazon.com/images/I/71J-YtSUd3L._AC_UX679_.jpg"
+          image="https://m.media-amazon.com/images/I/91oiSVwU7OL._AC_UL320_.jpg"
         />
       </div>
       <div className="home__row">
         <Product
           id="0003"
-          title="Color Club Halographic Hues Nail Polish, Halo, Graphic.05 Ounce"
-          price={11.29}
+          title="AmazonBasics Kids Easy-Wash Microfiber Bed-in-a-Bag Bedding Set - Twin, Dino Friends"
+          price={35.29}
           rating={4}
-          image="https://m.media-amazon.com/images/I/71FPuDUhfML._AC_UL480_QL65_.jpg"
+          image="https://m.media-amazon.com/images/I/91qQYFih2KL._AC_UL320_.jpg"
         />
         <Product
           id="0004"
-          title="Hamilton Beach Breakfast Sandwich Maker, Silver (25475A) "
-          price={24.99}
+          title="AmazonBasics Controller Charging Station for PlayStation 4 DualShock 4 Controllers - 4 Foot Cable, Black"
+          price={18.99}
           rating={4}
-          image="https://images-na.ssl-images-amazon.com/images/I/41N8AxIJq4L._AC_SY200_.jpg"
+          image="https://m.media-amazon.com/images/I/81dS49Qdn8L._AC_UL320_.jpg"
         />
         <Product
           id="0005"
-          title="Artist's Loft Acrylic Paint Value Pack 36 Piece"
-          price={25.35}
+          title="AmazonBasics Portable Wireless, 2.1 Bluetooth Speaker, Black"
+          price={28.99}
           rating={5}
-          image="https://m.media-amazon.com/images/I/61GEW-MekwL._AC_UL480_QL65_.jpg"
+          image="https://m.media-amazon.com/images/I/71VlL2uonUL._AC_UL480_FMwebp_QL65_.jpg"
         />
       </div>
       <div className="home__row">
         <Product
           id="0006"
-          title="FLY2SKY 28pcs Mochi Squishy Toys Mini Squishies Kawaii Animal Squishys Party Favors Easter Egg Fillers Easter Gifts for Kids Unicorn Cat Panda Animal Squeeze Toy Stress Relief Toy Class Prize, Random "
-          price={8.99}
+          title="AmazonBasics Blue, White, and Yellow Microfiber Cleaning Cloth - Pack of 24"
+          price={15.99}
           rating={4}
-          image="https://m.media-amazon.com/images/I/61ZA1VRxLOL._AC_UL480_QL65_.jpg"
+          image="https://m.media-amazon.com/images/I/81wUWVAiWDL._AC_UL480_FMwebp_QL65_.jpg"
         />
         <Product
           id="0007"
-          title="Oxford Spiral Notebooks, 1 Subject, College Ruled Paper, Durable Plastic Cover, 100 Sheets, Divider Pocket, 3 per Pack (10390) "
-          price={9.89}
+          title="AmazonBasics 8-Sheet Capacity, Cross-Cut Paper and Credit Card Shredder, 4.1 Gallon "
+          price={42.41}
           rating={5}
-          image="https://images-na.ssl-images-amazon.com/images/I/41OJa46VMDL._AC_US327_QL65_.jpg"
+          image="https://m.media-amazon.com/images/I/71K7Q4FpguL._AC_UL480_FMwebp_QL65_.jpg"
         />
       </div>
     </div>
